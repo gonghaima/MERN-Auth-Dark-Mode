fd1461fd5537d3a840e51e04751741d4a3977eea
Steven Gong <gonghaima@hotmail.com>
2023-04-25 17:11:04 +1000

    AdvertWidget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,7 +4,7 @@ import Navbar from 'scenes/navbar';
 import UserWidget from 'scenes/widgets/UserWidget';
 import MyPostWidget from 'scenes/widgets/MyPostWidget';
 import PostsWidget from 'scenes/widgets/PostsWidget';
-// import AdvertWidget from "scenes/widgets/AdvertWidget";
+import AdvertWidget from 'scenes/widgets/AdvertWidget';
 // import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
@@ -30,7 +30,11 @@ const HomePage = () => {
           <MyPostWidget picturePath={picturePath} />
           <PostsWidget userId={_id} />
         </Box>
-        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+          </Box>
+        )}
       </Box>
     </Box>
   );
